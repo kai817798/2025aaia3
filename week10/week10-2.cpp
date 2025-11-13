f740754fd412f3bc674d95f02417da0e173bc5d6
kai817798 <hyk20060817@gmail.com>
2025-11-13 17:19:43 +0800

    week10

---
+++ week10/week10-2.cpp
@@ -0,0 +1,17 @@
+//week10-2.cpp(埃程程キА羱)
+//LeetCode 1491.Average Salary Excluding the Minimum and Maximum Salary
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        double total = 0;//р羱常癬ㄓ
+        int N = salary.size();//羆ΤN
+        int M = salary[0],m = salary[0];//р材0讽程程
+        for(int i=0;i<N;i++){
+            total += salary[i];//р羱常癬ㄓ
+            if(salary[i]>M) M = salary[i];//ゑ程程传
+            if(salary[i]<m) m = salary[i];//ゑ程程传
+        }
+        //return total / N;//ぃ钡场埃璶Ι奔程程
+        return(total - M - m) / (N-2);//埃2(程程)埃
+    }
+};
